4e32ffaacd6f7ffa2e035bf7d6ac7c40d2e2c1a8
queirozdaniel <danielqueirozak77@gmail.com>
2024-01-24 01:01:11 -0300

    feat: adding random phrase to description

--- components/ActorsList.vue
+++ components/ActorsList.vue
@@ -32,11 +32,11 @@
                             <div v-if="!configStore.isAnonymous" class="font-bold">{{ actor.name }}</div>
                             <div v-else class="flex flex-row items-center">
                                 <span class="flex flex-row items-center" v-for="skeleton in getAnonymousSkeleton(actor.name)" :key="skeleton.prefix">
-                                    <p>{{ skeleton.prefix }} </p>
+                                    <p class="font-bold">{{ skeleton.prefix }} </p>
                                     <span class="skeleton h-4 rounded-none" :class="skeleton.widthClass"></span>
                                 </span>
                             </div>
-                            <div class="text-sm opacity-50">Bro faz sol</div>
+                            <div class="text-sm opacity-50"> {{ phrases[Math.floor(Math.random() * 10 + 1)] }}</div>
                         </div>
                     </div>
                     </td>
@@ -45,7 +45,7 @@
                     </td>
                     <td v-else class="min-w-48 flex flex-row items-center">
                         <span class="flex min-h-12 items-center" v-for="skeleton in getAnonymousSkeleton(actor.name)" :key="skeleton.prefix">
-                            <span>{{ skeleton.prefix }} </span>
+                            <span class="font-bold">{{ skeleton.prefix }} </span>
                             <span :class="skeleton.widthClass" class="skeleton h-4 rounded-none" ></span>
                         </span>
                     </td>
@@ -61,13 +61,19 @@
 const configStore = useConfigurationStore();
 const store = useDiscordStore()
 
-//methods
+const phrases = ref([
+        "Brow, faz sol", "Eee o que?", "Teu amor me pegou",
+        "Geladeira Tsunami!!", "Escreve e sai correndo...",
+        "Deixou chegar", "Tem que aturar", "E o pix, nada ainda ?",
+        "Reeeal, me dá um real", "OooOh Potência!"
+    ])
 
+// methods
 const getAnonymousSkeleton = (name: string) => {
     return name.split(" ").map(n => {
         return {
             prefix: n.charAt(0),
-            widthClass: `w-${n.substring(1).length  * 2 + 2}`,
+            widthClass: `w-${n.substring(1).length  * 2}`,
         }
     })
 }
